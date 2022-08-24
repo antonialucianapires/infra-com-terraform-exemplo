05afd687faa3b7f53a90968b4ba4ac4b6d4557a4
Antonia Luciana Pires <46853268+antonialucianapires@users.noreply.github.com>
2022-08-24 17:47:30 -0300

    criação da segunda subnet

--- vpc.tf
+++ vpc.tf
@@ -13,3 +13,12 @@ resource "aws_subnet" "fc-subnet-1" {
     "Name" = "fcsubnet-1"
   }
 }
+
+resource "aws_subnet" "fc-subnet-2" {
+  availability_zone = "us-east-1a"
+  vpc_id = aws_vpc.vpc-fc.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    "Name" = "fcsubnet-2"
+  }
+}
