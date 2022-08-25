54dccfe904df06868b06f29ae18c71f1fdbc0f58
Antonia Luciana Pires <46853268+antonialucianapires@users.noreply.github.com>
2022-08-25 14:26:53 -0300

    criação do internet gateway e route table

--- vpc.tf
+++ vpc.tf
@@ -8,12 +8,32 @@ resource "aws_vpc" "vpc-fc" {
 data "aws_availability_zones" "available" {}
 
 resource "aws_subnet" "subnets" {
-  count = 2
-  availability_zone = data.aws_availability_zones.available.names[count.index]
-  vpc_id = aws_vpc.vpc-fc.id
-  cidr_block = "10.0.${count.index}.0/24"
+  count                   = 2
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  vpc_id                  = aws_vpc.vpc-fc.id
+  cidr_block              = "10.0.${count.index}.0/24"
   map_public_ip_on_launch = true
   tags = {
     "Name" = "${var.prefix}-subnet-${count.index}"
+  }
+}
+
+resource "aws_internet_gateway" "fc-igw" {
+  vpc_id = aws_vpc.vpc-fc.id
+  tags = {
+    "Name" = "${var.prefix}-igw"
+  }
+}
+
+resource "aws_route_table" "fc-rt" {
+  vpc_id = aws_vpc.vpc-fc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.fc-igw.id
+  }
+
+  tags = {
+    Name = "${var.prefix}-rt"
   }
 }
