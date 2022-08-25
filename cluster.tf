a70e8d0ce878f4fa94169b81ae86e680e91f5fd5
Antonia Luciana Pires <46853268+antonialucianapires@users.noreply.github.com>
2022-08-25 14:53:32 -0300

    criação de role de acesso ao eks

--- cluster.tf
+++ cluster.tf
@@ -11,3 +11,21 @@ resource "aws_security_group" "sg-fc" {
     "Name" = "${var.prefix}-sg"
   }
 }
+
+resource "aws_iam_role" "role-cluster-fc" {
+  name               = "${var.prefix}-${var.cluster_name}-role"
+  assume_role_policy = <<POLICY
+    {
+       "Version": "2012-10-17",
+       "Statement": [
+            {
+                "Effect": "Allow",
+                "Principal": {
+                    "Service": "eks.amazonaws.com"
+                },
+                "Action": "sts:AssumeRole"
+            }
+       ] 
+    }
+  POLICY
+}
