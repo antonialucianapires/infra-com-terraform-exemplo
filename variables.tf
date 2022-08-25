52fceb163c1893576ca0f788ba8fae81b8820907
Antonia Luciana Pires <46853268+antonialucianapires@users.noreply.github.com>
2022-08-25 15:04:45 -0300

    criar variáveis
    - variável para nome do cluster 
    - variável para informar total de dias para retenção de logs

--- variables.tf
+++ variables.tf
@@ -1,3 +1,11 @@
 variable "prefix" {
   description = "prefixo para nome dos recursos"
 }
+
+variable "cluster_name" {
+  description = "nome do cluster"
+}
+
+variable "retention_days" {
+  description = "total de dias de retenção de logs"
+}
