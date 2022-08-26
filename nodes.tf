2f32f949df40fb12b1781b0a19d61093c4883118
Antonia Luciana Pires <46853268+antonialucianapires@users.noreply.github.com>
2022-08-26 12:35:32 -0300

    criação de role node eks

---
+++ nodes.tf
@@ -0,0 +1,17 @@
+resource "aws_iam_role" "node" {
+  name = "${var.prefix}-${var.cluster_name}-role-node"
+  assume_role_policy = <<POLICY
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Principal": {
+                    "Service": "ec2.amazonaws.com"
+                },
+                "Action": "sts:AssumeRole"
+            }
+        ]
+    }
+  POLICY
+}
